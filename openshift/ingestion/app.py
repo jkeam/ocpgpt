d4fb42cb1c1fba4fa895d6f5cf2e8704360464db
Jon Keam <jpkeam@gmail.com>
2023-08-10 14:53:07 -0400

    add ingestion webhook

---
+++ openshift/ingestion/app.py
@@ -0,0 +1,16 @@
+from parliament import Context, event
+from os import environ
+from json import loads
+from dotenv import load_dotenv
+load_dotenv()
+
+@event
+def main(context: Context) -> dict[str, Any]:
+    sender_email_address  = environ['SENDER_EMAIL_ADDRESS']
+    sender_email_password = environ['SENDER_EMAIL_PASSWORD']
+
+    data = loads(context.cloud_event.data)
+    receiver  = data['recipient']
+    body      = "Hello there from the PyMailer Function as a Service, running in OpenShift using OpenShift Serverless Functions."
+
+    return { "message": receiver }
