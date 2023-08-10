a4793d598a2099556168f7356860fb8b8d81ba27
Jon Keam <jpkeam@gmail.com>
2023-08-10 19:17:08 -0400

    add type hints

--- openshift/ingestion/app.py
+++ openshift/ingestion/app.py
@@ -1,32 +1,33 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, Response
 import uuid
 
+# from typing import Any
 # from os import environ
 # from json import loads
 from dotenv import load_dotenv
-# from typing import Any
 load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def health():
+def health() -> Response:
     return make_response({
         "message": "ok",
         "status": "healhty"
     })
 
 @app.route('/', methods=['POST'])
-def main():
-    app.logger.warning(request.data)
-    # Respond with another event (optional)
+def main() -> Response:
+    data = request.data
+    app.logger.debug(data)
+
     response = make_response({
         "message": "hi"
     })
     response.headers["Ce-Id"] = str(uuid.uuid4())
     response.headers["Ce-specversion"] = "0.3"
-    response.headers["Ce-Source"] = "knative/eventing/samples/hello-world"
-    response.headers["Ce-Type"] = "dev.knative.samples.hifromknative"
+    response.headers["Ce-Source"] = "keam/io/ai/ocpgpt"
+    response.headers["Ce-Type"] = "keam.io.ai.ocpgpt"
     return response
 
 if __name__ == '__main__':
